92f5c8cf06d5ba18f40cb815d2b7c694a6155ea4
fsdfsdfsdfdsf <72997426+fsdfsdfsdfdsf@users.noreply.github.com>
2022-10-17 16:40:10 +0200

    Add files via upload

---
+++ dc zajebiecizbani#0422/start.bat
@@ -0,0 +1,3 @@
+@echo
+
+start cmd.exe /k "npm i && node Tiktok-System-JS.js"
---
+++ dc zajebiecizbani#0422/tiktokrequests.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 16 Paź 2022, 20:50
+-- Wersja serwera: 10.4.25-MariaDB
+-- Wersja PHP: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `tiktokrequests`
+--
+
+CREATE TABLE `tiktokrequests` (
+  `type` text NOT NULL,
+  `value` bigint(22) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Zrzut danych tabeli `tiktokrequests`
+--
+
+INSERT INTO `tiktokrequests` (`type`, `value`) VALUES
+('like', 0),
+('gifty', 0),
+('totallikes', 0);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
